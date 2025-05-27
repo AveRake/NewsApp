f0a000dae78aada4ad646a4b0a6a6d0640da2fcd
Vadim Arslanov <averake@MacBook-Pro-Vadim.local>
2025-05-27 21:31:13 +0300

    Initial commit

---
+++ app/src/main/java/com/fedoseevdev/newsapp/utils/Util.kt
@@ -0,0 +1,11 @@
+package com.fedoseevdev.newsapp.utils
+
+object Util {
+
+    const val APP_NAME = "NEWSAPP"
+    const val API_BASE_URL = "http://newsapi.org/"
+    const val API_KEY = "de3cd105be344801a6bad4be297e9104"
+
+    const val COUNTRY_PREF = "countryPref"
+
+}
